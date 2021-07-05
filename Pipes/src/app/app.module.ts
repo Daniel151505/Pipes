0f37cb37d479bf385b74d22f26d752ccc8a99ec2
Daniel151505 <daniel150520011@gmail.com>
2021-07-04 22:48:44 -0500

    Importando Modulos para el cambio de idioma

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { registerLocaleData } from '@angular/common';
+import LocalEs from '@angular/common/locales/es'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(LocalEs)
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
