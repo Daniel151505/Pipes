1031efcdad5712f73a9a546ef1bb5c734996af62
Daniel151505 <daniel150520011@gmail.com>
2021-07-04 23:10:42 -0500

    Importando idioma

--- Pipes/src/app/app.module.ts
+++ Pipes/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { registerLocaleData } from '@angular/common';
 import LocalEs from '@angular/common/locales/es'
+import LocalFr from '@angular/common/locales/fr'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 registerLocaleData(LocalEs)
+registerLocaleData(LocalFr)
 
 @NgModule({
   declarations: [
